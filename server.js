8bdbaa9f5c7905732d4762e1bb8e9d7eba6eba47
XIQUQIX <sun.rongj@northeastern.edu>
2025-11-06 20:51:20 -0500

    fix server cors base URL prob

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 // 手动配置 CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  //res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://huskyhub.netlify.app/');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
