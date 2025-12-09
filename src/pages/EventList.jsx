3201897e505718948defd6b4c2a5132f116fe537
XIQUQIX <sun.rongj@northeastern.edu>
2025-12-09 16:13:49 -0500

    updated Proptype

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import './EventList.css';
 import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -581,5 +582,11 @@ function EventList() {
     </div>
   );
 }
+
+EventList.propTypes = {
+};
+
+EventList.defaultProps = {
+};
 
 export default EventList;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from "jwt-decode";
 import './Homepage.css';
+import PropTypes from 'prop-types';
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -407,5 +408,8 @@ function EventCard({ event, formatDate, getCategoryEmoji, isCreator = false, isP
     </div>
   );
 }
+
+Homepage.propTypes = {
+};
 
 export default Homepage;
